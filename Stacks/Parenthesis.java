934ab20fc976a3127355f2bddf90d231aa257bc8
Mihir Kate <mihirkate13@gmail.com>
2023-07-24 21:04:52 +0530

    Added

--- Stacks/Parenthesis.java
+++ Stacks/Parenthesis.java
@@ -3,23 +3,39 @@ import java.util.*;
 
 public class Parenthesis {
 
-public static boolean isParanthesis(String str){
-    Stack <Character> s=new Stack<>();
-    for(int i=0;i<str.length();i++){
-        char ch=str.charAt(i);
-        //opening 
-        if(ch=='('|| ch=='['|| ch=='{'){
-            s.push(ch);
+    public static boolean isParanthesis(String str) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // opening
+            if (ch == '(' || ch == '[' || ch == '{') {
+                s.push(ch);
+            } else {
+                // closing
+                if (s.isEmpty()) {
+                    return false;
+                }
+                if ((s.peek() == '(' && ch == ')') ||
+                        (s.peek() == '{' && ch == '}')
+                        || (s.peek() == '[' && ch == ']')) {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
         }
-        else{
-            
+        if (s.isEmpty()) {
+            return true;
+        } else {
+            return false;
         }
     }
-}
 
     public static void main(String[] args) {
-        String str = "(({}))";
-        isParanthesis(str);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Paranthesis :");
+        String str = sc.nextLine();
+        System.out.println(isParanthesis(str));
 
     }
 }
