dbc1dbe744ac0f4b041a8bb520fa0fa1bb6a7200
Mihir Kate <mihirkate13@gmail.com>
2023-08-11 23:10:47 +0530

    added poped element

--- Queues/StackUsingQueue.java
+++ Queues/StackUsingQueue.java
@@ -41,8 +41,8 @@ public class StackUsingQueue {
         s.push(2);
         s.push(3);
         while (!s.isEmpty()) {
-            System.out.println(" " + s.peek());
-            s.pop();
+            System.out.println(" poped element is " +
+                    s.pop());
         }
     }
 }
