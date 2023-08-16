a5c486ec91672dabc9cdcbcd85c96461ec3ab62d
Mihir Kate <mihirkate13@gmail.com>
2023-08-17 01:02:17 +0530

    k

--- LRU.c
+++ LRU.c
@@ -1,86 +1,104 @@
 #include <stdio.h>
-int findLRU(int time[], int n)
+#include <stdlib.h>
+#include <math.h>
+
+// Number of data points
+#define N 10
+
+// Number of clusters
+#define K 2
+
+// Maximum number of iterations
+#define MAX_ITER 100
+
+// Data points
+float data[N][2] = {{2, 3}, {3, 4}, {10, 12}, {8, 9}, {20, 25}, {25, 30}, {30, 35}, {80, 85}, {82, 80}, {90, 91}};
+
+// Cluster centers
+float centers[K][2];
+
+// Cluster assignments for each data point
+int assignments[N];
+
+// Function to compute distance between two points
+float distance(float x1, float y1, float x2, float y2)
 {
-    int i, minimum = time[0], pos = 0;
-    for (i = 1; i < n; ++i)
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+// Function to assign each data point to the nearest cluster
+void assignClusters()
+{
+    for (int i = 0; i < N; i++)
     {
-        if (time[i] < minimum)
+        float minDist = distance(data[i][0], data[i][1], centers[0][0], centers[0][1]);
+        assignments[i] = 0;
+
+        for (int j = 1; j < K; j++)
         {
-            minimum = time[i];
-            pos = i;
+            float dist = distance(data[i][0], data[i][1], centers[j][0], centers[j][1]);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                assignments[i] = j;
+            }
         }
     }
-    return pos;
 }
+
+// Function to update cluster centers
+void updateCenters()
+{
+    int counts[K] = {0};
+    float sumX[K] = {0};
+    float sumY[K] = {0};
+
+    for (int i = 0; i < N; i++)
+    {
+        int cluster = assignments[i];
+        sumX[cluster] += data[i][0];
+        sumY[cluster] += data[i][1];
+        counts[cluster]++;
+    }
+
+    for (int j = 0; j < K; j++)
+    {
+        if (counts[j] > 0)
+        {
+            centers[j][0] = sumX[j] / counts[j];
+            centers[j][1] = sumY[j] / counts[j];
+        }
+    }
+}
+
 int main()
 {
-    int no_of_frames, no_of_pages, frames[10], pages[30], counter = 0, time[10], flag1, flag2, i, j, pos, faults = 0;
-    float hit_ratio, miss_ratio;
-    printf("Enter number of frames: ");
-    scanf("%d", &no_of_frames);
-    printf("Enter number of pages: ");
-    scanf("%d", &no_of_pages);
-    printf("Enter reference string: ");
-    for (i = 0; i < no_of_pages; ++i)
+    // Initialize cluster centers randomly
+    for (int i = 0; i < K; i++)
     {
-        scanf("%d", &pages[i]);
-    }
-    for (i = 0; i < no_of_frames; ++i)
-    {
-        frames[i] = -1;
+        centers[i][0] = data[i][0];
+        centers[i][1] = data[i][1];
     }
 
-    for (i = 0; i < no_of_pages; ++i)
+    // Run K-Means algorithm
+    for (int iter = 0; iter < MAX_ITER; iter++)
     {
-        flag1 = flag2 = 0;
-
-        for (j = 0; j < no_of_frames; ++j)
-        {
-            if (frames[j] == pages[i])
-            {
-                counter++;
-                time[j] = counter;
-                flag1 = flag2 = 1;
-                break;
-            }
-        }
-
-        if (flag1 == 0)
-        {
-            for (j = 0; j < no_of_frames; ++j)
-            {
-                if (frames[j] == -1)
-                {
-                    counter++;
-                    faults++;
-                    frames[j] = pages[i];
-                    time[j] = counter;
-                    flag2 = 1;
-                    break;
-                }
-            }
-        }
-
-        if (flag2 == 0)
-        {
-            pos = findLRU(time, no_of_frames);
-            counter++;
-            faults++;
-            frames[pos] = pages[i];
-            time[pos] = counter;
-        }
-
-        printf("\n");
-
-        for (j = 0; j < no_of_frames; ++j)
-        {
-            printf("%d\t", frames[j]);
-        }
+        assignClusters();
+        updateCenters();
     }
-    printf("\n\nTotal no of hits = %d", (no_of_pages - faults));
-    hit_ratio = ((float)(no_of_pages - faults) / (no_of_pages)) * 100;
-    miss_ratio = 100 - hit_ratio;
-    printf("\nThe hit ratio is:%f", hit_ratio);
-    printf("\nThe miss ratio is:%f", miss_ratio);
+
+    // Print final cluster assignments and centers
+    printf("Final cluster assignments:\n");
+    for (int i = 0; i < N; i++)
+    {
+        printf("Data point (%.1f, %.1f) belongs to cluster %d\n", data[i][0], data[i][1], assignments[i]);
+    }
+
+    printf("\nFinal cluster centers:\n");
+    for (int j = 0; j < K; j++)
+    {
+        printf("Cluster %d center: (%.1f, %.1f)\n", j, centers[j][0], centers[j][1]);
+    }
+
     return 0;
 }
