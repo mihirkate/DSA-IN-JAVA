205c7258f06ec090a5176420d5fb21ed2d1cf521
Mihir Kate <mihirkate13@gmail.com>
2023-08-28 17:13:20 +0530

    Initial

--- Strings in Java/LargestString.java
+++ Strings in Java/LargestString.java
@@ -1,20 +1,23 @@
 public class LargestString {
     public static void main(String[] args) {
-        String fruits[] = { "apple", "a","mango", "banana" ,"kela","zebra"};
+        String fruits[] = { "apple", "a", "mango", "banana", "kela", "zebra" };
         String largest = fruits[0];
         for (int i = 1; i < fruits.length; i++) {
             if (largest.compareTo(fruits[i]) < 0) {
                 largest = fruits[i];
             }
-
+            if (largest == null) {
+                return;
+            }
         }
         System.out.println(largest);
-        String smallest=fruits[0];
+        String smallest = fruits[0];
         for (int i = 0; i < fruits.length; i++) {
-            if (smallest.compareTo(fruits[fruits.length-1]) >0) {
+            if (smallest.compareTo(fruits[fruits.length - 1]) > 0) {
                 smallest = fruits[i];
             }
 
-        }System.out.println(smallest);
+        }
+        System.out.println(smallest);
     }
 }
