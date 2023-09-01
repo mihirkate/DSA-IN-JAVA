f1abac153160c45a69d79a2a2a495b713077c1d5
Mihir Kate <mihirkate13@gmail.com>
2023-09-01 23:38:39 +0530

    modified binary

--- Queues/BinaryNoInQueues.java
+++ Queues/BinaryNoInQueues.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -13,6 +14,21 @@ public class BinaryNoInQueues {
             q.add(s1 + "1");
 
         }
+    }
+
+    static ArrayList<String> generate(int n) {
+        ArrayList<String> ans = new ArrayList<>();
+        Queue<String> q = new LinkedList<>();
+        q.add("1");
+        while (n-- > 0) {
+            String s1 = q.remove();
+            // q.remove();
+            // System.out.println(s1);
+            ans.add(s1);
+            q.add(s1 + "0");
+            q.add(s1 + "1");
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
