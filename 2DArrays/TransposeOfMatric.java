8f7a2a84c0891c2df4dd60159ec538da2a00066f
Mihir Kate <mihirkate13@gmail.com>
2023-09-08 01:09:27 +0530

    Transpose Of 2D array

---
+++ 2DArrays/TransposeOfMatric.java
@@ -0,0 +1,36 @@
+
+public class TransposeOfMatric {
+    public static void transpose(int matrix[][]) {
+        int row = 4;
+        int col = 4;
+        printMatrix(matrix);
+        int transpose[][] = new int[row][col];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+            System.out.println();
+        }
+        System.out.println("the  Transposed matrix is  ");
+        printMatrix(transpose);
+    }
+
+    public static void printMatrix(int matrix[][]) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 27, 29, 37, 48 },
+                { 32, 33, 39, 50 } };
+
+        transpose(matrix);
+    }
+}
