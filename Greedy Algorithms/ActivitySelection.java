71f1c1b4f55f98a068ff7ab0689d05eb2c2023b3
Mihir Kate <mihirkate13@gmail.com>
2023-09-08 00:35:35 +0530

    Started Learning about  Greedy Algorithms

---
+++ Greedy Algorithms/ActivitySelection.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ActivitySelection {
+    public static ArrayList<Integer> selectActivity(int arr[]) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int start[] = { 10, 20, 7 };
+        int end[] = { 20, 30, 40 };
+    }
+}
