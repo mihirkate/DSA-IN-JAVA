9307cc84f0d48d9e6b7a56a56ebe7fe5985fa318
Mihir Kate <mihirkate13@gmail.com>
2023-09-11 21:38:02 +0530

    Added Chocola Problem

---
+++ Greedy Algorithms/ChocolaProblem.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+/*
+ * for (int i = 0; i < horVer.length; i++) {
+ * System.out.print(" " + horVer[i]);
+ * }
+ * System.out.println();
+ * for (int i = 0; i < costVer.length; i++) {
+ * System.out.print(" " + costVer[i]);
+ * }
+ */
+public class ChocolaProblem {
+    public static void chocola(Integer costVer[], Integer costHor[]) {
+        Arrays.sort(costVer, Collections.reverseOrder());
+        Arrays.sort(costHor, Collections.reverseOrder());
+
+        // Horizontal cut-->h, Vertical Cut--->v
+        int h = 0, v = 0;
+        int hp = 1, vp = 1;
+        int cost = 0;
+        while (h < costHor.length && v < costVer.length) {
+            // cut for hozrizontal
+            if (costVer[v] <= costHor[h]) {
+                // horixzontal cut;
+                cost += (costHor[h] * vp);
+                hp++;
+                h++;
+            } else {
+                // vertical cut
+                cost += costVer[v] * hp;
+                vp++;
+                v++;
+            }
+        }
+        while (h < costHor.length) {
+            cost += (costHor[h] * vp);
+            hp++;
+            h++;
+        }
+        while (v < costVer.length) {
+            cost += costVer[v] * hp;
+            vp++;
+            v++;
+        }
+        System.out.println("Cost is " + cost);
+    }
+
+    public static void main(String[] args) {
+        Integer costVer[] = { 2, 1, 3, 1, 4 };
+        Integer costHor[] = { 4, 1, 2 };
+        chocola(costVer, costHor);
+    }
+}
