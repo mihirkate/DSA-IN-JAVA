a0d3a436a55559af614ba9f571f413e67b478950
Mihir Kate <mihirkate13@gmail.com>
2023-09-15 20:58:01 +0530

    Added Binary Trees

---
+++ Binary Trees/BuildTreePreOrder.java
@@ -0,0 +1,36 @@
+public class BuildTreePreOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int node[]) {
+            idx++;
+            if (node[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(node[idx]);
+            newNode.left = buildTree(node);
+            newNode.right = buildTree(node);
+            return newNode;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree b = new BinaryTree();
+        Node root = b.buildTree(nodes);
+        System.out.println("The Root of the Tree is " + root.data);
+    }
+}
