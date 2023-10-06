baf2314be061c25b9b5b40fdfeec8304dd37772a
Mihir Kate <mihirkate13@gmail.com>
2023-10-06 23:51:09 +0530

    added LowestCommonAncestor

--- Binary Trees/LowestAncestor.java
+++ Binary Trees/LowestAncestor.java
@@ -12,64 +12,39 @@ public class LowestAncestor {
         }
     }
 
-    public static boolean rootToNodePath(Node root, int node, ArrayList<Node> path) {
+    public static boolean getpath(Node root, int n, ArrayList<Node> path) {
         if (root == null) {
             return false;
         }
-
         path.add(root);
-
-        if (root.data == node) {
+        if (root.data == n) {
             return true;
         }
-
-        if (root.left != null && rootToNodePath(root.left, node, path)) {
+        boolean foundLeft = getpath(root.left, n, path);
+        boolean foundRight = getpath(root.right, n, path);
+        if (foundLeft || foundRight) {
             return true;
         }
-        if (root.right != null && rootToNodePath(root.right, node, path)) {
-            return true;
-        }
-
         path.remove(path.size() - 1);
         return false;
+
     }
 
     public static Node lca(Node root, int n1, int n2) {
         ArrayList<Node> path1 = new ArrayList<>();
         ArrayList<Node> path2 = new ArrayList<>();
-        rootToNodePath(root, n1, path1);
-        rootToNodePath(root, n2, path2);
-
+        // getting the path
+        getpath(root, n1, path1);
+        getpath(root, n2, path2);
+        // finding the Lowest common ancestor
         int i = 0;
         for (; i < path1.size() && i < path2.size(); i++) {
-            if (!path1.get(i).equals(path2.get(i))) {
+            if (path1.get(i) != path2.get(i)) {
                 break;
             }
         }
-
-        return path1.get(i - 1);
-    }
-
-    // Approach 2 - Find first node which found n1 & n2 in it's subtrees
-    public static Node lca2(Node root, int n1, int n2) {
-        if (root == null) {
-            return null;
-        }
-
-        if (root.data == n1 || root.data == n2) {
-            return root;
-        }
-
-        Node leftFound = lca2(root.left, n1, n2);
-        Node rightFound = lca2(root.right, n1, n2);
-
-        if (leftFound == null) {
-            return rightFound;
-        } else if (rightFound == null) {
-            return leftFound;
-        } else {
-            return root;
-        }
+        Node lca = path1.get(i - 1);
+        return lca;
     }
 
     public static void main(String args[]) {
@@ -87,9 +62,6 @@ public class LowestAncestor {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-
-        Node lca = lca2(root, 4, 0);
-        if (lca != null)
-            System.out.println(lca.data);
+        System.out.println(lca(root, 4, 5).data);
     }
 }
