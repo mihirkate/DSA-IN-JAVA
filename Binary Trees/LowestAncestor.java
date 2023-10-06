31e0784c69efd6fa39415930eb3febc115316b48
Mihir Kate <mihirkate13@gmail.com>
2023-10-07 01:01:45 +0530

    Added min DIstance code

--- Binary Trees/LowestAncestor.java
+++ Binary Trees/LowestAncestor.java
@@ -47,6 +47,21 @@ public class LowestAncestor {
         return lca;
     }
 
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+        if (leftLca == null) {
+            return rightLca;
+        }
+        if (rightLca == null) {
+            return leftLca;
+        }
+        return root;
+    }
+
     public static void main(String args[]) {
         /*
          * 1
@@ -62,6 +77,6 @@ public class LowestAncestor {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        System.out.println(lca(root, 4, 5).data);
+        System.out.println(lca2(root, 4, 7).data);
     }
 }
---
+++ Binary Trees/MinimumDIstanceOfNode.java
@@ -0,0 +1,74 @@
+public class MinimumDIstanceOfNode {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+        if (leftLca == null) {
+            return rightLca;
+        }
+        if (rightLca == null) {
+            return leftLca;
+        }
+        return root;
+    }
+
+    public static int lcaDistance(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = lcaDistance(root.left, n);
+        int rightDist = lcaDistance(root.right, n);
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        // find LCA first
+        Node lca = lca2(root, n1, n2);
+
+        int dist1 = lcaDistance(lca, n1);
+        int dist2 = lcaDistance(lca, n2);
+        return dist1 + dist2;
+
+    }
+
+    public static void main(String[] args) {
+        /*
+         * 1
+         * / \
+         * 2 3
+         * / \ / \
+         * 4 5 6 7
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.right.right = new Node(8);
+        System.out.println(minDist(root, 4, 5));
+    }
+}
