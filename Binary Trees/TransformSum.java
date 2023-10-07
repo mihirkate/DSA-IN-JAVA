1a756378c1202552033868d8843caf4a25529da0
Mihir Kate <mihirkate13@gmail.com>
2023-10-08 00:50:40 +0530

    Added Transform sum

--- Binary Trees/TransformSum.java
+++ Binary Trees/TransformSum.java
@@ -11,6 +11,30 @@ public class TransformSum {
         }
     }
 
+    /*
+     * GFG Code
+     * int transform(Node root){
+     * if(root==null){
+     * return 0;
+     * }
+     * int leftchild = transform(root.left);
+     * // fir right child ko call karenge
+     * int rightChild = transform(root.right);
+     * 
+     * int data=root.data;
+     * 
+     * /* // int newLeft=root.left==null?0:root.left.data;
+     * // int newRight=root.right==null?0:root.right.data;
+     */
+    /*
+     * root.data=leftchild+rightChild;
+     * return data+root.data;
+     * }
+     * public void toSumTree(Node root){
+     * 
+     * transform(root);
+     * }
+     */
     public static int transform(Node root) {
         if (root == null) {
             return 0;
@@ -31,10 +55,11 @@ public class TransformSum {
         if (root == null) {
             return;
         }
-        System.out.print(" " + root.data);
+
+        System.out.print(root.data + " ");
         preOrder(root.left);
         preOrder(root.right);
-
+        /* return root.data; */
     }
 
     public static void main(String[] args) {
@@ -52,9 +77,8 @@ public class TransformSum {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-
         preOrder(root);
-        System.out.println(transform(root));
+        System.out.println("\nThe Transformed Sum of The Tree is " + transform(root));
         preOrder(root);
     }
 }
